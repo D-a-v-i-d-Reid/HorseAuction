f5edc3fb13cb9aa30085cd12c4ab47250b9c9ec0
David Reid <134351711+D-a-v-i-d-Reid@users.noreply.github.com>
2023-11-30 17:40:36 -0500

    added try catch

--- AuctionDbContext.cs
+++ AuctionDbContext.cs
@@ -9,30 +9,45 @@ public class AuctionDbContext : DbContext
     public DbSet<Bidder> Bidders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
     {
-        optionsBuilder.UseSqlite("Data Source=AuctionData.db");
+        try
+
+        {
+            optionsBuilder.UseSqlite("Data Source=AuctionData.db");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error configuring database: {ex.Message}");
+        }
+
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Bid>()
-            .HasOne(b => b.Horse)
-            .WithMany(h => h.Bids)
-            .HasForeignKey(b => b.HorseId);
+        try
+        {
+            modelBuilder.Entity<Bid>()
+                .HasOne(b => b.Horse)
+                .WithMany(h => h.Bids)
+                .HasForeignKey(b => b.HorseId);
 
-        modelBuilder.Entity<Bid>()
-            .HasOne(b => b.Bidder)
-            .WithMany()
-            .HasForeignKey(b => b.BidderId);
+            modelBuilder.Entity<Bid>()
+                .HasOne(b => b.Bidder)
+                .WithMany()
+                .HasForeignKey(b => b.BidderId);
 
-        modelBuilder.Entity<Horse>().HasData(
-           new Horse { HorseId = 1, HorseName = "Genetic", Age = 3, Color = "Red Dun", Description = "Easy Keeper. Fun to be Around", PerformanceType = "Western Pleasure", StartingBid = 3500m },
-           new Horse { HorseId = 2, HorseName = "Rowdy", Age = 4, Color = "Bay with a snip", Description = "Barn sour and will kick you", PerformanceType = "Reining", StartingBid = 5000m },
-           new Horse { HorseId = 3, HorseName = "Brownie", Age = 2, Color = "Brown", Description = "Very green, just started under saddle", PerformanceType = "Western Pleasure", StartingBid = 10000m }
-           );
+            modelBuilder.Entity<Horse>().HasData(
+               new Horse { HorseId = 1, HorseName = "Genetic", Age = 3, Color = "Red Dun", Description = "Easy Keeper. Fun to be Around", PerformanceType = "Western Pleasure", StartingBid = 3500m },
+               new Horse { HorseId = 2, HorseName = "Rowdy", Age = 4, Color = "Bay with a snip", Description = "Barn sour and will kick you", PerformanceType = "Reining", StartingBid = 5000m },
+               new Horse { HorseId = 3, HorseName = "Brownie", Age = 2, Color = "Brown", Description = "Very green, just started under saddle", PerformanceType = "Western Pleasure", StartingBid = 10000m }
+               );
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error on model building: {ex.Message}");
+            
+        }
     }
-
 
 
 }
