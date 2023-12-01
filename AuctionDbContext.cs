143541020feb7c6bf2b2272bfbd7511dbdc2eaa0
David Reid <134351711+D-a-v-i-d-Reid@users.noreply.github.com>
2023-12-01 04:28:13 -0500

    Logging Added

--- AuctionDbContext.cs
+++ AuctionDbContext.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 using System.Xml.Schema;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
 namespace HorseAuction;
+
 public class AuctionDbContext : DbContext
 {
+    private readonly ILogger<AuctionDbContext> _logger;
+
+    public AuctionDbContext(DbContextOptions<AuctionDbContext> options, ILogger<AuctionDbContext> logger)
+        : base(options)
+    {
+        _logger = logger; 
+
+       // Log.Logger = new LoggerConfiguration();
+          //    .WriteTo.Console
+          //    .WriteTo.File("logs/AppContext.log", rollingInterval: RollingInterval.Day)
+          //    .CreateLogger();
+    }
+
     public DbSet<Horse> Horses { get; set; }
     public DbSet<Bid> Bids { get; set; }
     public DbSet<Bidder> Bidders { get; set; }
@@ -11,16 +29,19 @@ public class AuctionDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         try
-
         {
             optionsBuilder.UseSqlite("Data Source=AuctionData.db");
-          //  optionsBuilder.EnableSensitiveDataLogging();
+
+            optionsBuilder.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddSerilog()));
+
+            optionsBuilder.EnableSensitiveDataLogging();
+
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error configuring database: {ex.Message}");
+            _logger.LogError($"Error configuring database:  {ex.Message}");
         }
-        
+
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -29,7 +50,7 @@ public class AuctionDbContext : DbContext
         {
             modelBuilder.Entity<Bid>()
             .HasOne(b => b.Horse)
-            .WithMany()
+            .WithMany(h => h.Bids)
             .HasForeignKey(b => b.HorseId);
 
 
@@ -43,11 +64,12 @@ public class AuctionDbContext : DbContext
                new Horse { HorseId = 2, HorseName = "Rowdy", Age = 4, Color = "Bay with a snip", Description = "Barn sour and will kick you", PerformanceType = "Reining", StartingBid = 5000m },
                new Horse { HorseId = 3, HorseName = "Brownie", Age = 2, Color = "Brown", Description = "Very green, just started under saddle", PerformanceType = "Western Pleasure", StartingBid = 10000m }
                );
+
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error on model building: {ex.Message}");
-            
+
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System.ComponentModel.Design;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace HorseAuction
 {
@@ -6,100 +11,113 @@ namespace HorseAuction
     {
         static void Main(string[] args)
         {
-
-            using (var dbContext = new AuctionDbContext())
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(@"C:\Users\David\Documents\GitHubRepository\Horse Auction\Auction Log File", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+               
+            using var loggerFactory = LoggerFactory.Create(builder =>
             {
-                Auction auction = new(dbContext);
-                               
-                {                                       
-                                                     
-                                              
-                    
-                    bool exitProgram = false;
+                builder.AddSerilog();
+            });
+            var logger = loggerFactory.CreateLogger<AuctionDbContext>();
 
-                    while (!exitProgram)
+            var optionsBuilder = new DbContextOptionsBuilder<AuctionDbContext>();
+            optionsBuilder.UseSqlite("Data Source=AuctionData.db");
+            var options = optionsBuilder.Options;
+
+            var dbContext = new AuctionDbContext(options, logger);
+                                             
+                     
+            Auction auction = new(dbContext);
+
+            bool exitProgram = false;
+
+            while (!exitProgram)
+            {
+                Console.WriteLine("1. View Horses");
+                Console.WriteLine("2. View Horse Details");
+                Console.WriteLine("3. Place Bid");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Enter Your Choice:  ");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+
+
+                    switch (choice)
                     {
-                        Console.WriteLine("1. View Horses");
-                        Console.WriteLine("2. View Horse Details");
-                        Console.WriteLine("3. Place Bid");
-                        Console.WriteLine("4. Exit");
-                        Console.WriteLine("Enter Your Choice:  ");
+                        case 1:
+                            auction.DisplayHorses();
+                            break;
 
-                        if (int.TryParse(Console.ReadLine(), out int choice))
-                        {
-                            switch (choice)
+                        case 2:
+                            Console.Write("Enter the Horse ID to view details:  ");
+                            if (int.TryParse(Console.ReadLine(), out int horseId))
                             {
-                                case 1:
-                                    auction.DisplayHorses();
-                                    break;
+                                auction.ViewHorseDetails(horseId);
 
-                                case 2:
-                                    Console.Write("Enter the Horse ID to view details:  ");
-                                    if (int.TryParse(Console.ReadLine(), out int horseId))
-                                    {
-                                        auction.ViewHorseDetails(horseId);
-
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid input. Please enter a number.");
-                                    }
-                                    break;
-
-                                case 3:
-                                    Console.Write("Enter the HorseID to Place a Bid:  ");
-                                    if (int.TryParse(Console.ReadLine(), out horseId))
-                                    {
-                                        Console.Write("Enter Your Username:  ");
-                                        string? bidderName = Console.ReadLine();
-
-                                        Console.Write("Enter the Bid Amount ");
-                                        if (decimal.TryParse(Console.ReadLine(), out decimal bidAmount))
-                                        {
-                                            auction.Placebid(horseId, bidderName, bidAmount);
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Invalid bid amount. Please enter a valid number.");
-                                        }
-
-
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Invalid Input. Please enter a number.");
-                                    }
-                                    break;
-                                case 4:
-                                    exitProgram = true;
-                                    Console.WriteLine("Exiting the program.");
-                                    break;
-                                default:
-                                    Console.WriteLine("Invalid Choice. Please enter a number between 1 and 4.");
-                                    break;
                             }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid Input. Please enter a number.");
-                        }
-                        Console.WriteLine();
+                            else
+                            {
+                                Console.WriteLine("Invalid input. Please enter a number.");
+                            }
+                            break;
+
+                        case 3:
+                            Console.Write("Enter the HorseID to Place a Bid:  ");
+                            if (int.TryParse(Console.ReadLine(), out horseId))
+                            {
+                                Console.Write("Enter Your Username:  ");
+                                string? bidderName = Console.ReadLine();
+
+                                Console.Write("Enter the Bid Amount ");
+                                if (decimal.TryParse(Console.ReadLine(), out decimal bidAmount))
+                                {
+                                    auction.Placebid(horseId, bidderName, bidAmount);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid bid amount. Please enter a valid number.");
+                                }
 
 
-
-
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Input. Please enter a number.");
+                            }
+                            break;
+                        case 4:
+                            exitProgram = true;
+                            Console.WriteLine("Exiting the program.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Choice. Please enter a number between 1 and 4.");
+                            break;
                     }
                 }
+                else
+                {
+                    Console.WriteLine("Invalid Input. Please enter a number.");
+                }
+                Console.WriteLine();
+
+
+
 
             }
 
 
-
-
-
-
         }
+
+
+
+
+
+
     }
 }
+
 
 
