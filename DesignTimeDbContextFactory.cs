99ef92a3e2e34bed7a1d66aff6a9123d753330ec
David Reid <134351711+D-a-v-i-d-Reid@users.noreply.github.com>
2023-12-24 03:44:41 -0500

    Added DesignTimeDbContextFactory

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseAuction
+{ 
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AuctionDbContext>
+    {
+        public AuctionDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AuctionDbContext>();
+            optionsBuilder.UseSqlite("Data Source=AuctionData.db");
+
+            // Create logger
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+            var factory = serviceProvider.GetService<ILoggerFactory>();
+            var logger = factory.CreateLogger<AuctionDbContext>();
+
+            return new AuctionDbContext(optionsBuilder.Options, logger);
+        }
+    }
+}
